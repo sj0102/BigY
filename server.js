1692a0a3b44580781ea9fbbadc1eba9b56bc9370
sj0102 <sungjae0102@gmail.com>
2017-01-06 15:42:29 +0900

    161204_2
    디렉토리 정리

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 // 사용 모듈 로드
 var express = require('express'); // 웹서버 사용 .
 var app = express();
-var fs = require('fs'); // 파일 로드 사용.
+var router = require('./router/main')(app);
 
 
-// 포트 설정
-app.listen(8080, function () { 
-console.log('Server Start .');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+ 
+var server = app.listen(8080, function(){
+    console.log("Express server has started on port 8080")
 });
 
-// 라우팅 설정
-app.get('/', function (req, res) { // 웹서버 기본주소로 접속 할 경우 실행 . ( 현재 설정은 localhost 에 3303 port 사용 : 127.0.0.1:3303 )
-fs.readFile('index.html', function (error, data) { // index.html 파일 로드 .
-if (error) {
-console.log(error);
-} else {
-res.writeHead(200, { 'Content-Type': 'text/html' }); // Head Type 설정 .
-res.end(data); // 로드 html response .
-} 
-});
-});
+app.use(express.static('public'));
